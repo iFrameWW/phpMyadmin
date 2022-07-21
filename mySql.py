667fa8f94122ec62599150c00c3b7aab43230ce0
iFrameWW <freamkung2545@gmail.com>
2022-07-21 23:42:16 +0700

    -

--- mySql.py
+++ mySql.py
@@ -19,3 +19,23 @@ mycursor = mydb.cursor()
 
 # mycursor.execute("CREATE TABLE payment (Credit_card_band VARCHAR(255), Credit_card_number VARCHAR(255), Credit_card_holder VARCHAR(255))")
 
+# sql = "INSERT INTO customers (first_name, last_name, phone_number, home_number, road_name, address) VALUES (%s, %s, %s, %s, %s, %s)"
+# val = [
+#     ('Wonsgathorn','Wanwong','0972355339','103/5','sukumvit','moo.5'),
+#     ('Kornkanok','Pobchan-ad','0973504742','111/1','highway 21','-'),
+#     ('David','Jhon','0978521478','121/7','highway 23','-'),
+#     ('Bau','ponlawat','0987521589','169','rong-had road','-'),
+#     ('Jhon','GATEWOOD','0999999999','169','rong-had road','-'),
+# ]
+# mycursor.executemany(sql, val)
+# mydb.commit()
+# print(mycursor.rowcount, "record was inserted.")
+
+# sql = "INSERT INTO restaurant (name, location, phone_number) VALUES (%s, %s, %s)"
+# val = [
+#     ('KFC','Lamthong Bangsaen','-')
+# ]
+# mycursor.executemany(sql, val)
+# mydb.commit()
+# print(mycursor.rowcount, "record was inserted.")
+
